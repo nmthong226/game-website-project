f65c754d865c4e71416402c821d85ab6a6049270
nmthong226 <nmthong226@gmail.com>
2024-07-27 20:14:52 +0700

    add nav bar + bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './components/Home/Home'
+import NavBar from './components/Navigation/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      Hello World!
-    </>
+    <div className=''>
+      <NavBar/>
+      <Home/>
+    </div>
   )
 }
 
