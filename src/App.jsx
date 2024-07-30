112dbd35dac73c762d4a1119535ca90ed18657ca
nmthong226 <nmthong226@gmail.com>
2024-07-30 22:30:54 +0700

    ui config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Feature from './components/Feature/Feature'
 import Home from './components/Home/Home'
 import NavBar from './components/Navigation/NavBar'
+import News from './components/News/News'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className=''>
+      <Sidebar/>
       <NavBar/>
       <Home/>
+      <News/>
+      <Feature/>
     </div>
   )
 }
