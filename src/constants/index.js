df0119a51e2d55e1bf8e50f406ddebbc88582187
nmthong226 <nmthong226@gmail.com>
2024-08-01 23:42:38 +0700

    add constant for context placeholder

---
+++ src/constants/index.js
@@ -0,0 +1,132 @@
+export const newsList = [
+    {
+        type: 'Tin tức',
+        name: "Cập Nhật Phiên Bản 1.2",
+        date: "01/08/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Anh Hùng Hội Tụ: Tham Gia Đấu Võ Nhận Quà Khủng",
+        date: "12/07/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Giải Đấu Bang Hội: Thiên Hạ Đệ Nhất Bang",
+        date: "13/07/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Truy Tìm Bí Kíp Võ Công: Nhận Ngay Phần Thưởng",
+        date: "01/07/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Lễ Hội Trung Thu: Tham Gia Nhận Quà Đặc Biệt",
+        date: "03/07/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Cuộc Đua Thiên Lý Mã: Chinh Phục Ngựa Huyền Thoại",
+        date: "11/07/2024"
+    },
+    {
+        type: 'Tin tức',
+        name: "Cuộc Thi Sáng Tạo Trang Phục Võ Hiệp Độc Đáo",
+        date: "21/07/2024"
+    },
+    {
+        type: 'Sự kiện',
+        name: "Tích Lũy Đăng Nhập Nhận Trang Phục Hiếm",
+        date: "01/07/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Tích Lũy Đăng Nhập Nhận Trang Phục Hiếm",
+        date: "01/07/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Anh Hùng Hội Tụ: Tham Gia Đấu Võ Nhận Quà Khủng",
+        date: "15/06/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Giải Đấu Bang Hội: Thiên Hạ Đệ Nhất Bang",
+        date: "20/06/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Truy Tìm Bí Kíp Võ Công: Nhận Ngay Phần Thưởng",
+        date: "25/06/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Lễ Hội Trung Thu: Tham Gia Nhận Quà Đặc Biệt",
+        date: "30/06/2024"
+    },
+    {
+        type: "Sự kiện",
+        name: "Cuộc Đua Thiên Lý Mã: Chinh Phục Ngựa Huyền Thoại",
+        date: "05/07/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Cập nhật phiên bản 1.0",
+        date: "10/06/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Hệ Thống Bang Hội Nâng Cấp Mới",
+        date: "15/06/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Bổ Sung Võ Công Mới Cho Các Môn Phái",
+        date: "20/06/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Cập Nhật Giao Diện Người Dùng Thân Thiện Hơn",
+        date: "25/06/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Hệ Thống Săn Boss Thế Giới Cải Tiến",
+        date: "30/06/2024"
+    },
+    {
+        type: "Tính năng",
+        name: "Nâng Cấp Hệ Thống PvP Và PvE",
+        date: "05/07/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Cập Nhật Phiên Bản 1.0",
+        date: "01/06/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Cách Tham Gia Sự Kiện Anh Hùng Hội Tụ",
+        date: "05/06/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Tham Gia Giải Đấu Bang Hội",
+        date: "10/06/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Truy Tìm Bí Kíp Võ Công",
+        date: "15/06/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Tham Gia Lễ Hội Trung Thu",
+        date: "20/06/2024"
+    },
+    {
+        type: "Hướng dẫn",
+        name: "Hướng Dẫn Tham Gia Cuộc Đua Thiên Lý Mã",
+        date: "25/06/2024"
+    }
+]
