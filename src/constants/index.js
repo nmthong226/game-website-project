f855e8ea6e6804b25cef33dc00bf4737844ffd2c
nmthong226 <nmthong226@gmail.com>
2024-08-10 23:14:17 +0700

    add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,45 @@
+import tl_icon from '/tl-icon.png';
+import tl_image from '/tl-char.png';
+import tl_name from '/tl-name.png';
+import tl_stats from '/tl-stats.png';
+import tl_skill from '/tl-skill.gif';
+
+import nd_icon from '/nd-icon.png';
+import nd_image from '/nd-char.png';
+import nd_name from '/nd-name-2.png';
+import nd_stats from '/nd-stats.png';
+import nd_skill from '/nd-skill.gif';
+
+import mg_icon from '/mg-icon.png';
+import mg_image from '/mg-char.png';
+import mg_name from '/mg-name.png';
+import mg_stats from '/mg-stats.png';
+import mg_skill from '/mg-skill.gif';
+
+import vd_icon from '/vd-icon.png';
+import vd_image from '/vd-char.png';
+import vd_name from '/vd-name.png';
+import vd_stats from '/vd-stats.png';
+import vd_skill from '/vd-skill.gif';
+
+import nm_icon from '/nm-icon.png';
+import nm_image from '/nm-char.png';
+import nm_name from '/nm-name.png';
+import nm_stats from '/nm-stats.png';
+import nm_skill from '/nm-skill.gif';
+
+import cb_icon from '/cb-icon.png';
+import cb_image from '/cb-char.png';
+import cb_name from '/cb-name.png';
+import cb_stats from '/cb-stats.png';
+import cb_skill from '/cb-skill.gif';
+
+import tn_icon from '/tn-icon.png';
+import tn_image from '/tn-char.png';
+import tn_name from '/tn-name.png';
+import tn_stats from '/tn-stats.png';
+import tn_skill from '/tn-skill.gif';
+
 export const newsList = [
     {
         type: 'Tin tức',
@@ -128,5 +170,71 @@ export const newsList = [
         type: "Hướng dẫn",
         name: "Hướng Dẫn Tham Gia Cuộc Đua Thiên Lý Mã",
         date: "25/06/2024"
+    }
+]
+
+export const characterList = [
+    {
+        index: 0,
+        image: cb_image,
+        icon: cb_icon,
+        name: cb_name,
+        element: 'Hỏa',
+        stats: cb_stats,
+        skill: cb_skill,
+    },
+    {
+        index: 1,
+        image: tn_image,
+        icon: tn_icon,
+        name: tn_name,
+        element: 'Hỏa',
+        stats: tn_stats,
+        skill: tn_skill,
+    },
+    {
+        index: 2,
+        image: tl_image,
+        icon: tl_icon,
+        name: tl_name,
+        element: 'Thổ',
+        stats: tl_stats,
+        skill: tl_skill,
+    },
+    {
+        index: 3,
+        image: mg_image,
+        icon: mg_icon,
+        name: mg_name,
+        element: 'Mộc',
+        stats: mg_stats,
+        skill: mg_skill,
+    },
+    {
+        index: 4,
+        image: nd_image,
+        icon: nd_icon,
+        name: nd_name,
+        element: 'Mộc',
+        stats: nd_stats,
+        skill: nd_skill,
+    },
+    {
+        index: 5,
+        image: vd_image,
+        icon: vd_icon,
+        name: vd_name,
+        element: 'Thổ',
+        stats: vd_stats,
+        skill: vd_skill,
+    },
+    {
+        index: 6,
+        image: nm_image,
+        icon: nm_icon,
+        name: nm_name,
+        element: 'Thủy',
+        stats: nm_stats,
+        skill: nm_skill,
     }
 ]
