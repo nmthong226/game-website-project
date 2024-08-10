c95fb995e7df982ba6a46935446e152d0afbcd2d
nmthong226 <nmthong226@gmail.com>
2024-08-10 23:14:24 +0700

    optimize code

--- src/utils/DynamicImage.jsx
+++ src/utils/DynamicImage.jsx
@@ -1,46 +1,55 @@
 import React, { useState, useEffect } from 'react';
 
-const DynamicImage = ({ src, alt, originalWidth, originalHeight, ratio, className }) => {
-    const [dimensions, setDimensions] = useState({
-        width: originalWidth,
-        height: originalHeight
+
+const DynamicImage = ({
+  src,
+  alt,
+  originalWidth,
+  originalHeight,
+  ratio,
+  className,
+  style
+}) => {
+  const [imageDimensions, setImageDimensions] = useState({
+    width: originalWidth,
+    height: originalHeight
+  });
+
+  const scaleImage = () => {
+    const screenWidth = window.innerWidth;
+    const scaleFactor = screenWidth / originalWidth * ratio;
+    const newWidth = originalWidth * scaleFactor;
+    const newHeight = originalHeight * scaleFactor;
+
+    setImageDimensions({
+      width: newWidth,
+      height: newHeight
     });
+  };
 
-    const scaleImage = () => {
-        const screenWidth = window.innerWidth;
-        const scaleFactor = screenWidth / originalWidth * ratio;
-        const newWidth = originalWidth * scaleFactor;
-        const newHeight = originalHeight * scaleFactor;
+  useEffect(() => {
+    scaleImage();
 
-        setDimensions({
-            width: newWidth,
-            height: newHeight
-        });
+    window.addEventListener("resize", scaleImage);
+
+    return () => {
+      window.removeEventListener("resize", scaleImage);
     };
+  }, [originalWidth]);
 
-    useEffect(() => {
-        // Initial scaling
-        scaleImage();
-
-        // Re-scale the image when the window is resized
-        window.addEventListener('resize', scaleImage);
-
-        // Cleanup event listener on component unmount
-        return () => window.removeEventListener('resize', scaleImage);
-    }, [originalWidth]);
-
-    return (
-        <img
-            src={src}
-            alt={alt}
-            style={{
-                width: `${dimensions.width}px`,
-                height: `${dimensions.height}px`
-            }}
-            className={className}
-            draggable="false"
-        />
-    );
+  return (
+    <img
+      src={src}
+      alt={alt}
+      style={{
+        width: `${imageDimensions.width}px`,
+        height: `${imageDimensions.height}px`,
+        ...style
+      }}
+      className={className}
+      draggable="false"
+    />
+  );
 };
 
 export default DynamicImage;
